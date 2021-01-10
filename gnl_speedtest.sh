be3e7d324fcfcd3c868732709131c412dd81294b
mclam <mclam@student.21-school.ru>
2021-01-10 17:29:12 +0300

    some interface changes

--- gnl_speedtest.sh
+++ gnl_speedtest.sh
@@ -23,16 +23,17 @@ clear
 #for BUFSIZE in 32 42 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1058576 2097152 4194304 8388608 16777216
 for BUFSIZE in 4096
 do
-echo "####### Testing "$TESTEXT" with BUFFER_SIZE="$BUFSIZE
-rm -rf $OUT $OUT.dSYM
+echo -e "####### Testing \033[0;36myour GNL BUFFER_SIZE="$BUFSIZE
+echo -e "\033[0mfile: "$TESTEXT
 gcc $FLAGS=$BUFSIZE $CFILES $SPEEDTEST -o $OUT
 time $OUT $TESTEXT
 rm -rf $OUT $OUT.dSYM
 echo
 done
 
-echo -e "\033[0;36m####### Testing\033[0m "$TESTEXT" \033[0;36mwhith a.out_by_jbelinda"
+echo -e "####### Testing \033[0;36ma.out_by_jbelinda\033[0m"
+echo -e "file: "$TESTEXT
 time ./a.out_by_jbelinda $TESTEXT
 echo
-echo -e "\033[0mTest completed"
+echo -e "Test completed"
 echo
